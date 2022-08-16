bdbb4f6558b5c4d19853b46f22f9776902ca2354
tommyzhng <tommyz2005@gmail.com>
2022-08-15 23:32:27 -0400

    added character selection animations

--- Assets/Scenes/Menu/Scripts/sceneTransfer.cs
+++ Assets/Scenes/Menu/Scripts/sceneTransfer.cs
@@ -12,32 +12,75 @@ public class sceneTransfer : MonoBehaviour
 
     //Plane Selection
     public GameObject podium;
+    private GameObject item;
     public List<GameObject> planeList;
-    private Vector3 podiumDest = new Vector3(-4.5f, 0f, 0f);
-    float movePoint = 0f;
+    private int planeIndex = 0;
+
+    public Button next;
+    public Button prev;
+
     private void Start()
     {
         planeSelection.enabled = false;
-    }
-    private void FixedUpdate()
-    {
-        Podium();
+
     }
     //Podium Functions
-    private void Podium()
+    public void NextPlane()
     {
-        if (planeSelection.enabled && movePoint < 1)
-        {
-            movePoint += 0.25f * Time.deltaTime;
-            podium.transform.position = Vector3.Lerp(podium.transform.position, podiumDest, movePoint);
-        }
+        StopAllCoroutines();
+        StartCoroutine(MoveOut(planeList[planeIndex]));
+        planeIndex = (planeIndex + 1) % planeList.Count;
+        StartCoroutine(MoveIn(planeList[planeIndex], new Vector3(-4.5f, 1.25f, 0f)));
+
     }
+    public void PrevPlane()
+    {
+        StopAllCoroutines();
+        StartCoroutine(MoveOut(planeList[planeIndex]));
+        if (planeIndex == 0)
+        {
+            planeIndex = planeList.Count - 1;
+        }
+        else
+        {
+            planeIndex--;
+        }
+        StartCoroutine(MoveIn(planeList[planeIndex], new Vector3(-4.5f, 1.25f, 0f)));
+    }
+
+    public IEnumerator MoveIn(GameObject item, Vector3 dest)
+    {
+        float movePoint = 0f;
+        while (item.transform.position != dest)
+        {
+            item.transform.position = Vector3.Lerp(item.transform.position, dest, movePoint += .25f * Time.deltaTime);
+            yield return null;
+        }
+        movePoint = 0f;
+    }
+    public IEnumerator MoveOut(GameObject item)
+    {
+        Vector3 dest = new Vector3(-31.24f, 0f, 6.75f);
+        float move = 0f;
+        while (item.transform.position != dest)
+        {
+            item.transform.position = Vector3.Lerp(item.transform.position, dest, move += .25f * Time.deltaTime);
+            yield return null;
+        }
+        move = 0f;
+    }
+
     //When start button is clicked
     public void startGame()
     {
         startScreen.enabled = false;
         planeSelection.enabled = true;
+
+        StartCoroutine(MoveIn(podium, new Vector3(-4.5f, 0f, 0f)));
+        StartCoroutine(MoveIn(planeList[planeIndex], new Vector3(-4.5f, 1.25f, 0f)));
     }
+
+    //When tutorial clicked
     public void SpawnTutorial()
     {
         SceneManager.LoadScene("tutorial");

52295a9b5a6beabddadee7c9757a8ebe24baf488
tommyzhng <tommyz2005@gmail.com>
2022-08-27 16:40:37 -0400

    test

--- Assets/Scenes/-Scripts/Control/JoystickRotation.cs
+++ Assets/Scenes/-Scripts/Control/JoystickRotation.cs
@@ -24,23 +24,24 @@ public class JoystickRotation : MonoBehaviour
 
 	private void Update()
 	{
-
+		
 	}
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Hand"))
         {
+			Vector3 direction = other.transform.position - transform.position;
+			var rotation = Quaternion.LookRotation(direction).eulerAngles;
+			rotation.z = transform.rotation.z;
+			transform.localRotation = Quaternion.Euler(rotation);
 			if (pressed)
 			{
 				
 			}
+			
 		}
-		var direction = new Vector3(other.transform.position.x, other.transform.position.y, other.transform.position.z) - transform.position;
-		var rotation = Quaternion.LookRotation(direction);
-
-		rotation.eulerAngles = new Vector3(Mathf.Clamp(rotation.eulerAngles.x, 30, 330), rotation.eulerAngles.y, rotation.eulerAngles.z);
-		transform.rotation = rotation;
+		
 	}
 
     public void Grab(InputAction.CallbackContext gripPressed)

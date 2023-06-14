4a05adce7f69d279e96039d7518aaeee8e59f93b
tommyzhng <tommyz2005@gmail.com>
2023-06-14 17:31:07 -0400

    update gameobjects

--- Assets/Scenes/-Scripts/cameraPan.cs
+++ Assets/Scenes/-Scripts/cameraPan.cs
@@ -1,27 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class cameraPan : MonoBehaviour
 {
-    [SerializeField] float sensX = 0;
-    [SerializeField] float sensY = 0;
-    public GameObject Cam;
+    [SerializeField] float sensX = 2.2f;
+    [SerializeField] float sensY = 1.5f;
+    private GameObject Cam;
     Vector2 delta;
     Vector2 currentRot;
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.V))
         {
-            Cam.transform.localRotation = Quaternion.Euler(-currentRot.y, currentRot.x, 0);
+            transform.localRotation = Quaternion.Euler(-currentRot.y, currentRot.x, 0);
         }
-        else if (Cam.transform.localRotation != Quaternion.Euler(0, 0, 0)) 
+        else if (transform.localRotation != Quaternion.Euler(10, 0, 0)) 
         {
-            Cam.transform.localRotation = Quaternion.Slerp(Cam.transform.localRotation, Quaternion.Euler(0f, 0f, 0), 2f * Time.deltaTime);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(10f, 0f, 0), 2f * Time.deltaTime);
             currentRot.x = 0;
-            currentRot.y = 0;
+            currentRot.y = -10;
         }
     }
     public void panCamera(InputAction.CallbackContext mouseDelta)
@@ -30,7 +30,7 @@ public class cameraPan : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.Locked;
             delta = mouseDelta.ReadValue<Vector2>();
-            currentRot.x += delta.x * sensX *Time.deltaTime;
+            currentRot.x += delta.x * sensX * Time.deltaTime;
             currentRot.y += delta.y * sensY * Time.deltaTime;
             currentRot.x = Mathf.Repeat(currentRot.x, 360);
             currentRot.y = Mathf.Clamp(currentRot.y, -80, 80);
